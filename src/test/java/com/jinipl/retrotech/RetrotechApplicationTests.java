86489530c74d6c890b1c4ef257a07df36859ce2e
Java by Dawn <143783282+javabydawn@users.noreply.github.com>
2024-04-16 15:08:55 +0200

    Spring Boot default project files created.

---
+++ src/test/java/com/jinipl/retrotech/RetrotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinipl.retrotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
