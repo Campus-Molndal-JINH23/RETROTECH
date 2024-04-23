807222f47473a4c607eed836028ec67002436ccd
Samuel Andersson <samuel.andersson04@gmail.com>
2024-04-23 13:09:43 +0200

    Order och databas koppling.

--- src/main/java/com/jinipl/retrotech/RetrotechApplication.java
+++ src/main/java/com/jinipl/retrotech/RetrotechApplication.java
@@ -2,8 +2,10 @@ package com.jinipl.retrotech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class RetrotechApplication {
 
 	public static void main(String[] args) {
